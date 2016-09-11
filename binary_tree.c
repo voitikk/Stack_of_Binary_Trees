d1b1eed600f15bad7cf21ee024d8b32a774f6fd2
Alex Voitik <ajvoit17@g.holycross.edu>
2016-09-11 18:50:11 -0400

    Add files via upload

--- binary_tree.c
+++ binary_tree.c
@@ -1,91 +1,106 @@
-//Alex Voitik
-//Compiler Construction
-//Stack of Binary Trees
-//Last Edit: Sun, Sept. 11th
+/*
 
+Alex Voitik
+Compiler Construction
+Stack of Binary Trees
+Last Edit: Sun, Sept. 11th
 
+Stack and Binary Tree Functions based on code taken from
+        - http://www.cprogramming.com/tutorial/c/lesson18.html
+        - http://www.sanfoundry.com/c-program-stack-implementation/
+        
+Worked on this project with Kyle Quinn, we discussed how to visualize
+the problem, and then we helped each other when errors presented
+themselves.
 
-#include "stdio.h"
+*/
+
+#include <stdio.h>
 #include <stdlib.h>
 
 #define MAXSIZE  8
 
 //struct for the binary tree
-typedef struct treeNode
-{
+typedef struct treeNode{
         int data;
         struct treeNode *left;
         struct treeNode *right;
-
 }treeNode;
 
 //holds name and pointer to binary tree
-typedef struct stackElement
-{
-	
+typedef struct stackElement {
 	char name;
 	treeNode *bst;
-
 }stackElement;
 
 //the stack of binary trees
-struct stack
-{
+struct stack{
     stackElement stk[MAXSIZE];
     int top;
-
 };
-
 
 //define the stack
 typedef struct stack STACK;
 STACK s;
 
 //functions
+void initialize();
 void push(void);
 int  pop(void);
-void display(void);
 void clearBuffer(void);
-void initializeTree(void);
 treeNode * Insert(treeNode *node,int data);
-void PrintInorder(treeNode *node);
+int search(int key, treeNode *leaf);
 
 //main
 int main(){
+        
     s.top = -1;
     int finished = 1;
-
-	//menu options
+    initialize(); //initialize stack with name 'o' with one element, 8
     while(finished == 1){
         
         char option;
         
-		//FIX MENU ARGS
+		//Menu Args
         printf("Please Enter an Option\n");
-        printf("p(U)sh: Push an empth tree onto the stack\n");
+        printf("p(U)sh: Push an empty tree onto the stack\n");
         printf("p(O)p: Pop the top tree off the stack\n");
         printf("(A)dd: Add a symbol to the binary tree on the top of the stack\n");
         printf("(S)earch: Search for a symbol\n");
         printf("(Q)uit: Quit this program\n\n");
+        
         scanf("%c", &option);
         
         switch (option) {
             case 'q' : finished = 0; break;
-
-            case 'u' : printf("Option to Push\n"); push(); break;
                 
-            case 'o' : printf("Option to Pop\n"); pop(); break;
-				
-		    case 'd' : display(); break;
-
-				//case 'i' : initializeTree();
+            case 'u' : push(); break;
+                
+            case 'o' : pop(); break;
                 
 		    case 'a' : printf("Enter Value to add to the tree\n");
 				       int num;
 				       scanf("%d", &num);
-				       Insert(s.stk[s.top].bst, num); break;
-                
-		    case 's' : printf("Option to Search\n"); PrintInorder(s.stk[s.top].bst); break;
+				       s.stk[s.top].bst = Insert(s.stk[s.top].bst, num);
+                       break;
+                       
+		    case 's' : printf("What do you want to search for?\n");
+                       int item;
+                       int found = 0;
+                       int i = s.top;
+                       scanf("%d", &item);
+                       
+                       //loops through all the trees to find the item
+                       for(i; i >= 0; i--){
+                                if(search(item, s.stk[i].bst)){
+                                     printf("Item Found in tree %c\n", s.stk[i].name);
+                                     found = 1;
+                                }      
+                       }
+                       if(!found){
+                                printf("Item not found\n");
+                       }
+                       break;
                 
             default  : printf("Not Recognized\n\n"); break;
         }
@@ -94,134 +109,89 @@ int main(){
     printf("Done\n");    
 }
 
-//don't read in the escape
+//doesn't read in the escape
 void clearBuffer(){   
     while(getchar() != '\n');
 }
 
-
-/*****************************************************************************************
-Playing with binary trees */
-/*  http://www.cprogramming.com/tutorial/c/lesson18.html
-
-void initializeTree(){
-	treeNode *root = NULL; //root of the tree
-	root = Insert(root, 8);
-	root = Insert(root, 9);
-	root = Insert(root, 1);
-	PrintInorder(root);
-	return;
-
-	}*/
-
-//FOR DEBUGGING PURPOSES, WORKS
-void PrintInorder(treeNode *node)
-{
-        if(node==NULL)
-        {
-                return;
-        }
-        PrintInorder(node->left);
-        printf("%d ",node->data);
-        PrintInorder(node->right);
+//initialize the stack with one tree and one element
+void initialize(){
+	stackElement newElement;
+	treeNode *root = 0;
+    s.top = s.top + 1;
+    newElement.name = 'o'; //assign name to newElement
+    newElement.bst = Insert(root, 8); //bst points to the tree, initialized at 8
+    s.stk[s.top] = newElement;
 }
 
-//WORKS FINE
-treeNode * Insert(treeNode *node,int data)
-{
-        if(node==NULL)
-        {
+//pushes empty tree onto the stack
+void push(){
+
+	char name;
+	stackElement newElement;
+	treeNode *root = 0;
+
+    if (s.top == (MAXSIZE - 1)){
+        printf ("Stack is Full\n");
+        return;
+    }
+    else{
+        name = s.stk[s.top].name + 1;
+        s.top = s.top + 1;
+		newElement.name = name; //assign name to newElement
+		newElement.bst = 0;//bst points to the null tree
+		s.stk[s.top] = newElement;
+    }
+    return;
+}
+
+//Inserts a leaf, in sorted order, to the tree passes to it
+treeNode * Insert(treeNode *node,int data){
+        if(node==0){
                 treeNode *temp;
                 temp = (treeNode *)malloc(sizeof(treeNode));
                 temp -> data = data;
                 temp -> left = temp -> right = NULL;
                 return temp;
         }
-        if(data >(node->data))
-        {
+        if(data >(node->data)){
                 node->right = Insert(node->right,data);
         }
-        else if(data < (node->data))
-        {
+        else if(data < (node->data)){
                 node->left = Insert(node->left,data);
         }
         /* Else there is nothing to do as the data is already in the tree. */
         return node;
 }
 
-
-//http://www.sanfoundry.com/c-program-stack-implementation/
-
-//WORKS FINE
-void push(){
-	char name;
-	stackElement newElement;
-	treeNode *root = NULL;
-
-    if (s.top == (MAXSIZE - 1))
-    {
-        printf ("Stack is Full\n");
-        return;
-    }
-    else
-    {
-        printf ("Enter the name of the element to be pushed\n"); //gets name of the stack
-
-		getchar(); //chew up newline
-        name = getchar();
-        s.top = s.top + 1;
-
-		newElement.name = name; //assign name to newElement
-
-		newElement.bst = Insert(root, 8); //bst points to the tree, initialized at 8
-
-		printf("name = %c\n", newElement.name); //debugging
-		printf("data = %d\n", newElement.bst -> data); //debugging
-		//assign newElement to s.top (overload operator)
-		s.stk[s.top] = newElement;
-		printf("s.top = %d\n", (s.stk[s.top]).bst -> data); //debugging
-
-		//here, s.stk[s.top].bst is the pointer to the binary tree in top
-    }
-    return;
-}
-
-//WORKS FINE
-int pop ()
-{
+//Pops a tree off of the stack
+int pop (){
+        
     int num;
-    if (s.top == - 1)
-    {
+    if (s.top == - 1){
         printf ("Stack is Empty\n");
         return (s.top);
     }
-    else
-    {
+    else{
         num = s.top;
-        printf ("poped element is = %d\n", s.stk[s.top]);
         s.top = s.top - 1;
     }
     return(num);
 }
 
-//PROBABLY DON'T NEED
-void display ()
-{
-    int i;
-    if (s.top == -1)
-    {
-        printf ("Stack is empty\n");
-        return;
-    }
-    else
-    {
-        printf ("\n The names of the stack items are: \n");
-        for (i = s.top; i >= 0; i--)
-        {
-            printf ("%c\n", s.stk[i]);
-        }
-    }
-    printf ("\n");
+//Searches a tree for an element. If found, 1 is returned.
+//if not found, 0 is returned.
+int search(int key, treeNode *leaf){
+  if( leaf != 0 ){
+      if(key==leaf->data){
+          return 1;
+      }
+      else if(key<leaf->data){
+          return search(key, leaf->left);
+      }
+      else{
+          return search(key, leaf->right);
+      }
+  }
+  else return 0;
 }
-
-//NEED TO IMPLEMENT A SEARCH, BUT ONE IS GIVEN ON THE BINARY TREE SPEC
